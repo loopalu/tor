5090bd88c82846c2b4162dd314f9f6dfb08ed49d
haleit <haleit@ttu.ee>
2019-03-31 16:50:01 +0300

    Sending thread

--- Register/src/server/SendingThread.java
+++ Register/src/server/SendingThread.java
@@ -1,5 +1,18 @@
 package server;
 
-public class SendingThread {
+import java.io.BufferedReader;
 
+
+//new Thread(new SendingThread(in)).start();
+public class SendingThread implements Runnable {
+
+    private BufferedReader in;
+
+    public SendingThread(BufferedReader input) {
+        this.in = input;
+    }
+
+    public void run() {
+
+    }
 }
