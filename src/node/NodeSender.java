f036973584da2cd425be5967a910c3a3cd3584d7
mandzeete <aivar.loopalu@gmail.com>
2019-04-19 01:06:36 +0300

    Vähendasin TTL. Sender saab TTL config failist nüüd.

--- src/node/NodeSender.java
+++ src/node/NodeSender.java
@@ -1,6 +1,7 @@
 package node;
 
 import util.FileWritter;
+import util.PropertyReader;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -18,41 +19,49 @@ public class NodeSender implements Runnable {
      */
     public void run() {
         while (isRunning) {
-            Scanner reader = new Scanner(System.in);
-            System.out.println("Enter page: ");
-            String urlString = null;
+            PropertyReader propertyReader;
             try {
-                urlString = URLEncoder.encode(reader.nextLine(), "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            String time = String.valueOf(System.currentTimeMillis());
-            try {
-                FileWritter.write(String.valueOf(NodeController.getPort()), time, false);
+                int port = NodeController.getPort();
+                propertyReader = new PropertyReader(port + "config.properties");
+                int timeToLive = propertyReader.getTimeToLive();
+                Scanner reader = new Scanner(System.in);
+                System.out.println("Enter page: ");
+                String urlString = null;
+                try {
+                    urlString = URLEncoder.encode(reader.nextLine(), "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                String time = String.valueOf(System.currentTimeMillis());
+                try {
+                    FileWritter.write(String.valueOf(port), time, false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (NodeController.getNeighbours().size() >= 2) {
+                    for (String i : NodeController.getNeighbours()) {
+                        try {
+                            URL url = new URL(i);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                            conn.setRequestMethod("GET");
+                            conn.setRequestProperty("Content-Type", "text/plain");
+                            conn.setRequestProperty("url", urlString);
+                            conn.setRequestProperty("id", time);
+                            conn.setRequestProperty("timetolive", String.valueOf(timeToLive));
+                            conn.setDoOutput(true);
+
+                            Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    System.out.println("Pole piisavalt naabreid!");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            if (NodeController.getNeighbours().size() >= 2) {
-                for (String i : NodeController.getNeighbours()) {
-                    try {
-                        URL url = new URL(i);
-                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-                        conn.setRequestMethod("GET");
-                        conn.setRequestProperty("Content-Type", "text/plain");
-                        conn.setRequestProperty("url", urlString);
-                        conn.setRequestProperty("id", time);
-                        conn.setRequestProperty("timetolive", String.valueOf(5));
-                        conn.setDoOutput(true);
-
-                        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-                        in.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                System.out.println("Pole piisavalt naabreid!");
             }
         }
     }
